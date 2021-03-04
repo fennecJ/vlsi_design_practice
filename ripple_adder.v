439ae65c2a4903e7dd774337929587e58f1301d4
fennec <f44071055@gs.ncku.edu.tw>
2021-03-04 18:44:28 +0800

    Implement 5 Full-Adders and wire_c

--- ripple_adder.v
+++ ripple_adder.v
@@ -14,9 +14,14 @@ input [4:0] A,B;
 input addsub;
 output [4:0]S;
 output Cout, ov_flag;
-
+//wire
+wire [3:0] c;
 /************ your code **************/
-
+FullAdder FullAdder0(.A(A[0]),.B(D[0]),.Cin(1'b0),.S(S[0]),.Cout(c[0]));
+FullAdder FullAdder1(.A(A[1]),.B(D[1]),.Cin(c[0]),.S(S[1]),.Cout(c[1]));
+FullAdder FullAdder2(.A(A[2]),.B(D[2]),.Cin(c[1]),.S(S[2]),.Cout(c[2]));
+FullAdder FullAdder3(.A(A[3]),.B(D[3]),.Cin(c[2]),.S(S[3]),.Cout(c[3]));
+FullAdder FullAdder4(.A(A[4]),.B(D[4]),.Cin(c[3]),.S(S[4]),.Cout(Cout));
 
 /************************************/
 
