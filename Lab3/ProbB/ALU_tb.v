d6154daf648ec94bf42d63f471daf288390d56f9
fennecJ <hwahwa649@gmail.com>
2021-03-12 01:54:15 +0800

    Add signed attr to src1 src2

--- Lab3/ProbB/ALU_tb.v
+++ Lab3/ProbB/ALU_tb.v
@@ -10,7 +10,6 @@
 
 `include "ALU.v"
 // ---------------------- define ---------------------- //
-
 `define DataSize  32
 `define ALUopSize 5
 //define ALUop
@@ -35,12 +34,13 @@
 `define SRLUop    5'b10001
 
 module ALU_tb;
+reg [8*6:1] ops;
 
 // ----------------------   reg   ---------------------- //
 reg                     alu_enable;
 reg   [`ALUopSize-1:0]  alu_op;
-reg   [`DataSize-1:0]   src1;
-reg   [`DataSize-1:0]   src2;
+reg signed  [`DataSize-1:0]   src1;
+reg signed  [`DataSize-1:0]   src2;
 
 // ----------------------   wire  ---------------------- //
 wire   [`DataSize-1:0]  alu_out;
@@ -51,22 +51,22 @@ ALU alu (.alu_enable(alu_enable), .alu_op(alu_op), .src1(src1), .src2(src2),
 
 //monitor
 initial begin
-  $monitor("alu_enable = %b, alu_op = %b,\nsrc1    = %b,  | %d\nsrc2    = %b,  | %d\nalu_out = %b,alu_overflow = %b\n", alu_enable, alu_op, src1, src1, src2, src2,alu_out, alu_overflow);
+  $monitor("alu_enable = %b, alu_op = %b,  | %s\nsrc1    = %b,  | %d\nsrc2    = %b,  | %d\nalu_out = %b,alu_overflow = %b\n", alu_enable, alu_op, ops, src1, src1, src2, src2,alu_out, alu_overflow);
 end
 
 initial begin
-  alu_enable = 1; alu_op = `ADDop;  src1 = 32'hff0f_0f0f; src2 = 32'h8000_0000;
-#10               alu_op = `SUBop;  src1 = 32'h0f0f_0f0f; src2 = 32'h5000_0000;
-#10               alu_op = `ANDop;  src1 = 32'h0f0f_0f0f; src2 = 32'h000f_000f;
-#10               alu_op = `ORop;   src1 = 32'h0f0f_0f0f; src2 = 32'h00f0_00f0;
-#10               alu_op = `XORop;  src1 = 32'h0f0f_0f0f; src2 = 32'hf0f0_0f0f;
-#10               alu_op = `NORop;  src1 = 32'h0f0f_0f0f; src2 = 32'hf0f0_0f0f;
-#10               alu_op = `SRLop;  src1 = 32'hff0f_0f0f; src2 = 32'h0000_0002;
-#10               alu_op = `ROTRop; src1 = 32'h0f0f_0f0f; src2 = 32'h0000_0003;
-#10				  alu_op = `NOTop;  src1 = 32'h0f0f_0f0f; src2 = 32'h0000_0000;
-#10				  alu_op = `NANDop;  src1 = 32'haf3c_ff00; src2 = 32'h50c3_ff00;
-#10				  alu_op = `MAXop;  src1 = 32'h8000_1234; src2 = 32'h0fff_0111;
-#10				  alu_op = `NOTop;  src1 = 32'h0f0f_0f0f; src2 = 32'H0000_0000;
+  alu_enable = 1; alu_op = `ADDop;  src1 = 32'hff0f_0f0f; src2 = 32'h8000_0000;  ops = "ADDop";
+#10               alu_op = `SUBop;  src1 = 32'h0f0f_0f0f; src2 = 32'h5000_0000;  ops = "SUBpp";
+#10               alu_op = `ANDop;  src1 = 32'h0f0f_0f0f; src2 = 32'h000f_000f;  ops = "ANDop";
+#10               alu_op = `ORop;   src1 = 32'h0f0f_0f0f; src2 = 32'h00f0_00f0;  ops = "ORop";
+#10               alu_op = `XORop;  src1 = 32'h0f0f_0f0f; src2 = 32'hf0f0_0f0f;  ops = "XORop";
+#10               alu_op = `NORop;  src1 = 32'h0f0f_0f0f; src2 = 32'hf0f0_0f0f;  ops = "NORop";
+#10               alu_op = `SRLop;  src1 = 32'hff0f_0f0f; src2 = 32'h0000_0002;  ops = "SRLop";
+#10               alu_op = `ROTRop; src1 = 32'h0f0f_0f0f; src2 = 32'h0000_0003;  ops = "ROTRop";
+#10				  alu_op = `NOTop;  src1 = 32'h0f0f_0f0f; src2 = 32'h0000_0000;  ops = "NOTop";
+#10				  alu_op = `NANDop;  src1 = 32'haf3c_ff00; src2 = 32'h50c3_ff00; ops = "NANDop";
+#10				  alu_op = `MAXop;  src1 = 32'h8000_1234; src2 = 32'h0fff_0111;  ops = "MAXop";
+#10				  alu_op = `NOTop;  src1 = 32'h0f0f_0f0f; src2 = 32'H0000_0000;  ops = "NOTop";
 //  Done by students
 
 #10  $finish;
