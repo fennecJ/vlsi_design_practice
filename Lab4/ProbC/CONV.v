aac985281338e34d2ea3b72112811625b210f76f
fennec <f44071055@gs.ncku.edu.tw>
2021-03-27 01:43:29 +0800

    Fix some bugs in CONV.v

--- Lab4/ProbC/CONV.v
+++ Lab4/ProbC/CONV.v
@@ -36,31 +36,33 @@ module CONV(clk, rst, w_w, if_w, w_in0, w_in1, w_in2, w_in3, w_in4, w_in5, w_in6
       if(rst)
       out <= 17'b0;
       else
-      begin
-      if(w_w)begin
-          w_in[0]<=w_in0;
-          w_in[1]<=w_in1;
-          w_in[2]<=w_in2;
-          w_in[3]<=w_in3;
-          w_in[4]<=w_in4;
-          w_in[5]<=w_in5;
-          w_in[6]<=w_in6;
-          w_in[7]<=w_in7;
-          w_in[8]<=w_in8;
-      end
-      if(if_w)begin
-          if_in[0]<=if_in0;
-          if_in[1]<=if_in1;
-          if_in[2]<=if_in2;
-          if_in[3]<=if_in3;
-          if_in[4]<=if_in4;
-          if_in[5]<=if_in5;
-          if_in[6]<=if_in6;
-          if_in[7]<=if_in7;
-          if_in[8]<=if_in8;
-      end
-       out <= w_in[0]*if_in[0] + w_in[1]*if_in[1] + w_in[2]*if_in[2] + w_in[3]*if_in[3] + w_in[4]*if_in[4] + w_in[5]*if_in[5] + w_in[6]*if_in[6] + w_in[7]*if_in[7] + w_in[8]*if_in[8]; 
-	  end       
+       out <= w_in[0]*if_in[0] + w_in[1]*if_in[1] + w_in[2]*if_in[2] + w_in[3]*if_in[3] + w_in[4]*if_in[4] + w_in[5]*if_in[5] + w_in[6]*if_in[6] + w_in[7]*if_in[7] + w_in[8]*if_in[8];       
 	  //$display("%d ",w_in[0]*if_in[0] + w_in[1]*if_in[1] + w_in[2]*if_in[2] + w_in[3]*if_in[3] + w_in[4]*if_in[4] + w_in[5]*if_in[5] + w_in[6]*if_in[6] + w_in[7]*if_in[7] + w_in[8]*if_in[8]);
 	  end
+
+  always @(*)begin
+      if(w_w)begin
+          w_in[0]=w_in0;
+          w_in[1]=w_in1;
+          w_in[2]=w_in2;
+          w_in[3]=w_in3;
+          w_in[4]=w_in4;
+          w_in[5]=w_in5;
+          w_in[6]=w_in6;
+          w_in[7]=w_in7;
+          w_in[8]=w_in8;
+      end
+      if(if_w)begin
+          if_in[0]=if_in0;
+          if_in[1]=if_in1;
+          if_in[2]=if_in2;
+          if_in[3]=if_in3;
+          if_in[4]=if_in4;
+          if_in[5]=if_in5;
+          if_in[6]=if_in6;
+          if_in[7]=if_in7;
+          if_in[8]=if_in8;
+	  end
+end
+
   endmodule 
