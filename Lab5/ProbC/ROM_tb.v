6293618cfafe48c5e2ee978bf49fb25922e367b1
fennecJ <hwahwa649@gmail.com>
2021-03-31 01:27:12 +0800

    Lab5_C ROM now show the err in a concise way

--- Lab5/ProbC/ROM_tb.v
+++ Lab5/ProbC/ROM_tb.v
@@ -18,21 +18,32 @@ module ROM_tb;
   reg         read_enable;
   reg  [13:0]  address;
   wire [23:0] data_out;
-  integer i ;
+  integer i ,err;
   ROM rom1 (clk, address, read_enable, data_out);
 
   initial clk=1'b0;
   always #10	clk=~clk;
 
   initial begin
+        err = 0;
         clk = 0; rst = 0;
         read_enable = 0; address = 14'd0;
     #20 rst = 1;
     #40 rst = 0; read_enable = 1;
     // Please add some test pattern to verify your module
-    #20 for(i=0;i<16384;i=i+1000)
-        $display($time, " RAM[%d]=%h, ", i, rom1.memory[i]);
-    #20 $finish;
+    #20 for(i=0;i<16384;i=i+1)
+        if(rom1.memory[i]!=i) begin
+        $display($time, "ns Error at RAM[%d]=%h, ", i, rom1.memory[i]);
+        err = err + 1;
+        end
+    #20  if(err==0)begin
+    $display("****************************************************************");
+    $display("*                                                              *");
+    $display("*                Congradulations! All correct                  *");
+    $display("*                                                              *");
+    $display("****************************************************************");
+    end
+    $finish;
   end
   initial begin
   $readmemh("ROM_data.dat",rom1.memory);
