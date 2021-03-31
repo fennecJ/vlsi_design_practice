57662f01d7f2e04f5f9fa77a9711f9d3a19f9bc6
fennecJ <hwahwa649@gmail.com>
2021-03-31 10:53:50 +0800

    Add rst and clear instruction into Lab5_D

--- Lab5/ProbD/mac.v
+++ Lab5/ProbD/mac.v
@@ -1,4 +1,6 @@
 
+
+
 module mac(clk,rst,clear,w_w,w_in,if_w,if_in,out);
 
   // ---------------------- input  ---------------------- //
@@ -9,7 +11,6 @@ module mac(clk,rst,clear,w_w,w_in,if_w,if_in,out);
  input	if_w;	
  input [`DATA_BIT-1:0] w_in;
  input [`DATA_BIT-1:0] if_in;
- 
  // ---------------------- output  ---------------------- //
  output[`DATA_BIT*2+1:0]out;
  
@@ -17,8 +18,27 @@ module mac(clk,rst,clear,w_w,w_in,if_w,if_in,out);
  reg signed [`DATA_BIT-1:0] weight [2:0];
  reg signed [`DATA_BIT-1:0] feature [2:0];
 
+integer i;
  // ---------------------- Write down Your design below  ---------------------- //
+ always @(posedge clk or posedge rst or posedge clear)begin
+   if(rst)begin
+    out <= 18'b0;
+    for(i = 0; i < 3;i=i+1)begin
+      weight[i] <= `DATA_BIT'b0;
+      feature[i] <= `DATA_BIT'b0;
+    end
+   end
+    else if(clear)begin
+      for(i = 0; i < 3;i=i+1)begin
+      weight[i] <= `DATA_BIT'b0;
+      feature[i] <= `DATA_BIT'b0;
+      end
+    end
+    else begin
+
+    end
  
+ end
 
   
 endmodule
