5637f0a3b09bdfd3feb2035578607abfc43fbd83
fennecJ <hwahwa649@gmail.com>
2021-04-15 15:21:21 +0800

    Init Conv from Lab5 mac

--- Lab6/src/Conv.v
+++ Lab6/src/Conv.v
@@ -8,7 +8,8 @@
 /////////////////////////////////////////////////////////////////////
 
 `include "define.vh"
-
+`define RBITS `DATA_BITS*2+2
+`define TBITS `DATA_BITS*2
 module Conv(clk, rst, clear, w_w, w_in, if_w, if_in, result);
 
 // ---------------------- input  ---------------------- //
@@ -22,6 +23,45 @@ output [`DATA_BITS*2+1:0]  result;
 reg signed [`DATA_BITS-1:0]   weight [2:0];
 reg signed [`DATA_BITS-1:0]   feature [2:0];
 
+reg signed [`DATA_BITS*2+1:0] result;
+reg signed [`DATA_BITS*2-1:0] tmp;
+integer i;
 // ---------------------- Write down Your design below  ---------------------- //
+always @(posedge clk)begin
+   if(rst)begin
+    for(i = 0; i < 3;i=i+1)begin
+      weight[i] <= `DATA_BITS'b0;
+      feature[i] <= `DATA_BITS'b0;
+    end
+   end
+    else if(clear)begin
+      for(i = 0; i < 3;i=i+1)begin
+      weight[i] <= `DATA_BITS'b0;
+      feature[i] <= `DATA_BITS'b0;
+      end
+    end
+    else begin
+       if(w_w) begin
+      weight[2] <= weight[1];
+      weight[1] <= weight[0];
+      weight[0] <= w_in;
+      end
+    if(if_w) begin
+      feature[2] <= feature[1];
+      feature[1] <= feature[0];
+      feature[0] <= if_in;
+    end      
+    end
+ end
+ always @(*)begin
+   if(rst) begin
+   tmp = `TBITS'b0;
+   result = `RBITS'b0;
+   end
+   else begin
+   tmp = feature[2]*weight[2]+feature[1]*weight[1]+feature[0]*weight[0];
+   result = (tmp<0)?({2'b11,tmp}):({2'b0,tmp});
+   end
+ end
 
 endmodule
